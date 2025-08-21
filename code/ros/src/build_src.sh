e1d05d4ba6960887244e9ec9c5fefecc700f2ab4
kristblo <kristblo@stud.ntnu.no>
2025-08-21 12:41:02 +0200

    upd buildscript to include all packages

--- code/ros/src/build_src.sh
+++ code/ros/src/build_src.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 colcon build --packages-select orca_moveit_config orca_moveit_config
+colcon build --packages-select orca_ctrl_msgs
+colcon build --packages-select control_listener
 echo "built"
 source ./install/setup.bash
 echo "sourced"
